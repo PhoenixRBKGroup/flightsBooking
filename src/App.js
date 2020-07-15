c395c7be66042344b7ca59c578c0bb7faa0e47b4
Shoukri <saljarjawi@gmail.com>
2020-07-15 09:49:21 +0300

    sign form style

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 //import logo from "./logo.svg";
 import "./App.css";
-<<<<<<< HEAD
 import HomePage from "./components/HomePage/HomePage.js";
-import Signin from "./components/signIn/signIn.js";
+import Signin from "./components/signIn/signin.js";
 import Signup from "./components/signUp/signUP.js";
 
 // import AppComponent from "./test/saerchFlight.jsx";
@@ -16,16 +15,6 @@ function App() {
       <Route path="/Signin" component={Signin} />
       <Route path="/Signup" component={Signup} />
     </Switch>
-=======
-import Signin from "./components/signin/signin.js"
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi Rula</h1>
-      <Signin/>
-    </div>
->>>>>>> origin/master
   );
 }
 
